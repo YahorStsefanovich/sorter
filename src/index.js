468d1782fb36243c89d75b9bbed2d6b2990f9342
Yegor Stephanovich <stefanovich_egor@mail.ru>
2018-09-21 23:13:45 +0300

    Try to do comparator. Read README.md

--- src/index.js
+++ src/index.js
@@ -1,31 +1,64 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.sorter = [];
+        this.state = 1;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        let n = this.sorter.length;
+        this.sorter[n] = element;
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.sorter[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.sorter.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.sorter;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        if (this.compareFunction){
+            return this.sorter.sort(this.compareFunction);
+        }
+        let mass = [], j = 0;
+        for (let i = 0; i < indices.length; i++)
+        {
+            let index = indices[i];
+            if (this.sorter[index]) {
+                mass[j] = this.sorter[index];
+                j++;
+            }
+        }
+        mass.sort();
+        for (let i = 0; i < indices.length; i++)
+        {
+            let index = indices[i];
+            this.sorter[index] = mass[i];
+        }
+    }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
+
+obj = new Sorter();
+obj.add(4);
+obj.add(3);
+obj.add(7);
+obj.add(1);
+obj.add(12);
+obj.sort([0,1,2,3,4]);
+console.log(obj.toArray());
+const reverseCompareFunction = (left, right) => right - left;
+obj.setComparator(reverseCompareFunction);
+obj.sort([0,1,2,3,4]);
+console.log(obj.toArray());
 
 module.exports = Sorter;
